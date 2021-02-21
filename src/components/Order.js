da21956ef1fb2e0f342adf064cf4fd6f9cd434b4
jyromaniac <wilkieejiroghene@gmail.com>
2021-02-21 13:40:53 +0200

    init

---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Order extends React.Component {
+    render() {
+        return (
+            <p><Order></Order></p>
+        )
+    }
+}
+
+export default Order; 
