20973651e484b6f84308e927ed3d80dc118d520f
jyromaniac <wilkieejiroghene@gmail.com>
2021-03-02 22:12:38 +0200

    Added Inventory Live State editing

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
 
@@ -55,6 +56,12 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -84,7 +91,12 @@ class App extends React.Component {
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} params={this.props.match.params}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory
+         addFish={this.addFish}
+         loadSamples={this.loadSamples} 
+         fishes={this.state.fishes}
+         updateFish={this.updateFish}
+         />
       </div>
     )
   }
